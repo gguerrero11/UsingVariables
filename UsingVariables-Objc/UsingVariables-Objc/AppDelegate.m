f2c5fa5c5826cac329b13f3aa1264a95f3fc4411
gguerrero11 <gguerrero11@gmail.com>
2015-01-20 23:17:13 -0800

    Added Variables, Prints BMI is 25.799444

--- UsingVariables-Objc/UsingVariables-Objc/AppDelegate.m
+++ UsingVariables-Objc/UsingVariables-Objc/AppDelegate.m
@@ -16,10 +16,12 @@
             
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    // Insert code here...
+    CGFloat height = 71.0;
+    CGFloat weight = 185.0;
     
+    CGFloat bodyMassIndex = (weight / (height * height)) * 703;
     
+    NSLog(@"THe BMI is %f", bodyMassIndex);
     return YES;
 }
 
